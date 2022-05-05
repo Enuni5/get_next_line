527970ff348cda1d7b6962880304f525b41131f6
Emilio Nunez nieto <enunez-n@c2r8s3.42malaga.com>
2022-05-05 16:55:23 +0200

    keep the work in progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: enunez-n <enunez-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 12:08:56 by enunez-n          #+#    #+#             */
-/*   Updated: 2022/05/05 12:09:50 by enunez-n         ###   ########.fr       */
+/*   Updated: 2022/05/05 16:23:17 by enunez-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	*buffsave;
 
+	if (!fd || BUFFER_SIZE <= 0)
+		return (NULL);
 	buffsave = ft_save_buffer(fd, buffsave);
 	if (!buffsave)
 		return (NULL);
@@ -27,23 +29,25 @@ char	*get_next_line(int fd)
 
 char	*ft_save_buffer(int fd, char *buffsave)
 {
-	int			bytesr;
-	static char	*buffsave;
+	int		bytesread;
+	char	*tmp;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
 	if (!buffsave)
 		buffsave = ft_newstr();
-	bytesr = 1;
-	while (ft_strchr(buffsave, '\n') && bytesr != 0)
+	bytesread = 1;
+	tmp = malloc(BUFFER_SIZE + 1);
+	if (!tmp)
+		return (NULL);
+	while (!ft_strchr(buffsave, '\n') && bytesread != 0)
 	{
-		bytesr = read(fd, buffsave, BUFFER_SIZE);
-		if (bytesr < 0)
+		bytesread = read(fd, tmp, BUFFER_SIZE);
+		if (bytesread < 0)
 		{
-			free(buffsave);
+			free(tmp);
 			return (NULL);
 		}
-		buffsave[bytesr] = '\0';
+		buffsave = ft_strjoin(buffsave, tmp);
+		buffsave[bytesread] = '\0';
 	}
 	return (buffsave);
 }
@@ -53,4 +57,49 @@ char	*ft_get_line(char *buffsave)
 	char	*line;
 	size_t	i;
 
+	if (!buffsave)
+		return (NULL);
+	i = 0;
+	while (buffsave[i] != '\n' && buffsave[i])
+		i++;
+	line = malloc(i + 2);
+	if (!line)
+	{
+		free(buffsave);
+		return (NULL);
+	}
+	i = 0;
+	while (buffsave[i] != '\n' && buffsave[i])
+	{
+		line[i] = buffsave[i];
+		i++;
+	}
+	if (buffsave[i] == '\n')
+		line[i++] = '\n';
+	line[i] = 0;
+	return (line);
+}
+
+char	*ft_clean_save(char *buffsave)
+{
+	char	*cleaned_buffsave;
+	size_t	i;
+	size_t	j;
+
+	if (!buffsave)
+		return (NULL);
+	i = 0;
+	while (buffsave[i] && buffsave[i] != '\n')
+		i++;
+	cleaned_buffsave = malloc(ft_strlen(buffsave) - i + 1);
+	if (!cleaned_buffsave)
+	{
+		free(buffsave);
+		return (NULL);
+	}
+	j = 0;
+	while (buffsave[i] && buffsave[i] != '\n')
+		cleaned_buffsave[j++] = buffsave[i++];
+	cleaned_buffsave[j] = 0;
+	return (cleaned_buffsave);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: enunez-n <enunez-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 12:08:52 by enunez-n          #+#    #+#             */
-/*   Updated: 2022/05/05 12:08:26 by enunez-n         ###   ########.fr       */
+/*   Updated: 2022/05/05 15:55:15 by enunez-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,7 @@ char	*ft_strjoin(char *s1, char *s2);
 char	*ft_newstr(void);
 char	*get_next_line(int fd);
 char	*ft_save_buffer(int fd, char *buffsave);
+char	*ft_get_line(char *buffsave);
+char	*ft_clean_save(char *buffsave);
 
 #endif
